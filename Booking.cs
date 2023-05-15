f9b203d95524d179bd9982c6084900a503809435
Graeme Smith <S00227303@atu.ie>
2023-05-15 16:49:02 +0100

    First update, parts a-f done

---
+++ Booking.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODExam_GraemeSmith_S00227303
+{
+    public class Booking
+    {
+        public int BookingID { get; set; }
+        public DateTime BookingDate { get; set; }
+        public int NumberOfTickers { get; set; }
+        public Movie Movie { get; set; }
+    }
+}
---
+++ Movie.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace OODExam_GraemeSmith_S00227303
+{
+    public class Movie
+    {
+        public int MovieID { get; set; }
+        public string Title { get; set; }
+        public string ImageName { get; set; }
+        public string Description { get; set; }
+        public string Cast { get; set; }
+        public virtual ICollection<Booking> Bookings { get; set; }
+    }
+
+    public class MovieData : DbContext
+    {
+        public MovieData() : base("OODExam_GraemeSmith") { }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
